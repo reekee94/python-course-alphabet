d2dccd85bc8ecb4ae5817f2182fa63fc3aab8c2c
reekee94 <48716669+reekee94@users.noreply.github.com>
2019-05-08 13:43:10 +0300

    Update homework.py
    added test

--- objects_and_classes/homework/homework.py
+++ objects_and_classes/homework/homework.py
@@ -77,13 +77,15 @@ class Cesar:
         return self.hit_hat() <= other.hit_hat()
 
     def add_car(self, car, garage=None):
-        garage = garage or max(self.garages, key=lambda x: x.freeplaces)
+        garage = garage or max(self.garages, key=lambda x: x.freeplace)
         if garage.freeplace > 0:
             garage.add_car(car)
         else:
             print('No places free places in', garage)
 
-
+    @staticmethod
+    def compare_cesar(cesar_list: list):
+        return max((cesar for cesar in cesar_list), key=lambda x: x.hit_hat() )
 
 
 
@@ -134,27 +136,17 @@ class Car:
 
     def change_number(self):
         self.number = uuid.uuid4()
+        return self.number
 
 
 
 class Garage:
-    def __init__(self, town, places = 3, owner=None):
+    def __init__(self, town, places: int, garage_cars:[Car],  owner=None):
         self.town = town
-        self.places = int(places)
+        self.places = places
         self.owner = uuid.uuid4()
-        self.cars = []
+        self.cars = garage_cars if garage_cars is not None else []
         self.freeplace = self.places - len(self.cars)
-
-        @property
-        def town(self):
-            return self._town
-
-        @town.setter
-        def town(self, t):
-            if t not in TOWNS:
-                raise Exception('Types must be one of: {0}.'.format(TOWNS))
-            self._town = t
-
 
     def add_car(self, other):
         if other not in self.cars and len(self.cars) < self.places:
@@ -166,3 +158,107 @@ class Garage:
 
     def hit_hat(self):
          return sum(c.price for c in self.cars)
+
+
+
+cars = []
+for car_counter in range(random.randint(10, 100)):
+    car = Car(price=random.uniform(100.5, 999.5),
+              type=random.choice(CARS_TYPES),
+              producer=random.choice(CARS_PRODUCER),
+              mileage=random.uniform(0, 1000000),
+              )
+    cars.append(car)
+
+car1 = Car(price=random.uniform(100.5, 999.5),
+           type=random.choice(CARS_TYPES),
+           producer=random.choice(CARS_PRODUCER),
+           mileage=random.uniform(0, 1000000),
+           )
+car2 = Car(price=random.uniform(100.5, 999.5),
+           type=random.choice(CARS_TYPES),
+           producer=random.choice(CARS_PRODUCER),
+           mileage=random.uniform(0, 1000000),
+           )
+
+print ("CAR1: ", car1)
+print ("CAR2: ", car2)
+
+# CHECK UUID CHANGE
+print ("\n------CHECK UUID CHANGE--------")
+old_number = car1.number
+car1.number = car1.change_number()
+print("The number {} of car {} was changed on: {}".format(old_number, car1.producer, car1.number))
+
+# CHECK COMPARE
+print ("\n-----CHECK COMPARE-------")
+if car1.price <= car2.price:
+    print(
+        "price {} car {} biggest then price {} car {}".format(car2.price, car2.producer, car1.price, car1.producer))
+else:
+    print(
+        "price {} car {} biggest then price {} car {}".format(car1.price, car1.producer, car2.price, car2.producer))
+
+if car1.price < car2.price:
+    print(
+        "car {} price {} cheaper then car {} price {}".format(car1.producer, car1.price, car2.producer, car2.price))
+else:
+    print("car {} cheaper then car {}".format(car2.producer, car1.producer))
+
+if car1.price == car2.price:
+    print(
+        "price {} car {} is equal to price {} car {}".format(car1.price, car1.producer, car2.price, car2.producer))
+
+if car1.price != car2.price:
+    print("price car {} isn't equal to price car {}".format(car1.producer, car2.producer))
+
+
+print ("\n-----CHECK GARAGE------")
+CESAR_NAME = ["Oleg", "Vitaliya", "Marina", "Grisha", "Petr"]
+cesars = []
+for cesar_counter in range(random.randint(2, 3)):
+    garageslist = []
+    for garage_counter in range(random.randint(1, 3)):
+        count_cars = random.randint(1, 4)
+        random_place = random.randint(0, 10)
+
+        get_car = []
+        for i in random.sample(cars, count_cars):
+            if i not in get_car:
+                cars.remove(i)
+                get_car.append(i)
+
+            if count_cars > random_place:
+                raise ValueError("Count cars more then count place. RUN PGOGRAM AGAIN")
+            else:
+                garage_real = Garage(town=random.choice(TOWNS),
+                                places=random_place,
+                                garage_cars=get_car)
+            garageslist.append(garage_real)
+            print (garage_real)
+
+
+
+
+    cesar = Cesar(name=random.choice(CESAR_NAME), garages=garageslist)
+    cesars.append(cesar)
+
+print ("\n-----CHECK CESAR------")
+for item in cesars:
+    print(item, "\n")
+
+print ("THE RICHEST CESAR IS: ",(Cesar.compare_cesar(cesars)).name)
+
+print("\n----CHECK ADD CARS----------")
+print("BEFORE ADD NEW CAR: ", garageslist[0])
+print("PRICE ALL CARS IN GARAGE BEFORE ADD CAR: ", garageslist[0].hit_hat())
+
+print("\nAFTER ADD NEW CAR: ", garageslist[0].add_car(car1))
+print("PRICE ALL CARS IN GARAGE AFTER ADD CAR: ", garageslist[0].hit_hat())
+
+print("\nBEFORE REMOVE NEW CAR: ", garageslist[0])
+print("\nCAR FOR REMOVE: ", garageslist[0].cars[0])
+print("\nAFTER REMOVE NEW CAR: ", garageslist[0].remove_car(garageslist[0].cars[0]))
+
+print("\nADD CAR TO FREE GARAGE: ", cesars[0].add_car(car1))
+print("\nADD CAR TO SELECTED GARAGE: ", cesars[0].add_car(car2, garageslist[0]))
